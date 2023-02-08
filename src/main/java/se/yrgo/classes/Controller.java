f62199f5931181a8c7f654cdb97a2d75a543f71e
Adrian Åsling Sellius <adrian@gmail.com>
2023-02-08 14:31:53 +0100

    commenting Controller

--- src/main/java/se/yrgo/classes/Controller.java
+++ src/main/java/se/yrgo/classes/Controller.java
@@ -1,4 +1,16 @@
 package se.yrgo.classes;
 
 public class Controller {
+    // lista över använda bokstäver
+    // rätt ord från Reader
+    // stjärn-ord (skapa)
+    // guessed char från Game
+
+    // metod för att returnera stjärn-ordet som sträng
+    //
+
+    // fel bokstav
+    // // redan använd bokstav
+    // rätt bokstav
+    // färdig
 }
