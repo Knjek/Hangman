066484ba052424f4d502aa84ff69cfd9354ffd50
Adrian Åsling Sellius <adrian@gmail.com>
2023-02-08 14:50:58 +0100

    adding method checkLetter

--- src/main/java/se/yrgo/classes/Controller.java
+++ src/main/java/se/yrgo/classes/Controller.java
@@ -3,6 +3,26 @@ package se.yrgo.classes;
 public class Controller {
     // lista över använda bokstäver
     // rätt ord från Reader
+    private char[] correctWord;
+    private char[] blankWord;
+
+    public Controller (String correctWord) {
+        this.correctWord = correctWord.toCharArray();
+    }
+
+    public String checkLetter(char c) {
+        for (int i = 0; i < correctWord.length; i++) {
+            if (c == correctWord[i]) {
+                blankWord[i] = c;
+            }
+        }
+        return String.valueOf(blankWord);
+    }
+    private static void makeBlank() {
+
+    }
+
+
     // stjärn-ord (skapa)
     // guessed char från Game
 
