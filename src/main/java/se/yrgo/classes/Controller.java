40103607ffb03dc89da8e36c65ae92a2b55e94e8
Christopher Lo-Martire <chrlom0905@skola.goteborg.se>
2023-02-13 09:09:58 +0100

    Commiting

--- src/main/java/se/yrgo/classes/Controller.java
+++ src/main/java/se/yrgo/classes/Controller.java
@@ -1,4 +1,47 @@
 package se.yrgo.classes;
 
+import java.util.Arrays;
+
 public class Controller {
+    // lista över använda bokstäver
+    // rätt ord från Reader
+    private char[] correctWord;
+    private char[] blankWord;
+    private String latestGuessedWord;
+
+    public Controller (String correctWord) {
+        this.correctWord = correctWord.toCharArray();
+        makeBlank(correctWord);
+        latestGuessedWord = String.valueOf(blankWord);
+    }
+
+    public boolean checkLetter(char c) {
+        boolean isCorrect = false;
+        for (int i = 0; i < correctWord.length; i++) {
+            if (c == correctWord[i]) {
+                blankWord[i] = c;
+                isCorrect = true;
+            }
+        }
+        latestGuessedWord = String.valueOf(blankWord);
+        return isCorrect;
+    }
+    private void makeBlank(String word) {
+        blankWord = new char[word.length()];
+        Arrays.fill(blankWord, '*');
+    }
+
+    public String getLatestGuessedWord() {
+        return latestGuessedWord;
+    }
+// stjärn-ord (skapa)
+    // guessed char från Game
+
+    // metod för att returnera stjärn-ordet som sträng
+    //
+
+    // fel bokstav
+    // // redan använd bokstav
+    // rätt bokstav
+    // färdig
 }
--- src/main/java/se/yrgo/classes/Game.java
+++ src/main/java/se/yrgo/classes/Game.java
@@ -1,6 +1,72 @@
 package se.yrgo.classes;
 
+import java.util.Scanner;
+
 public class Game {
+    private int guessesleft;
+
+    private Controller control;
+    private Reader read;
+    private Scanner scan;
+    private String correctWord;
+
+
+    public Game(Scanner scan) {
+        this.scan = scan;
+        read = new Reader();
+        correctWord = "test";
+        control = new Controller(correctWord);
+        guessesleft = 8;
+
+    }
+
+    public void play() {
+
+        while (true) {
+            char guess = askQuestion();
+
+            checkAnswer(guess);
+
+            if (isWon()) break;
+
+            if (isOutOfGuesses()) break;
+        }
+
+    }
+
+    private char askQuestion() {
+        System.out.print("Guess one letter: ");
+        char guess = scan.nextLine().charAt(0);
+        return guess;
+    }
+
+    private void checkAnswer(char guess) {
+        if (control.checkLetter(guess)) {
+            System.out.println("You got one! gimmie another one");
+            System.out.println(control.getLatestGuessedWord());
+
+        } else {
+            System.out.println("Wrong guess, try again");
+            System.out.println(control.getLatestGuessedWord());
+            guessesleft--;
+        }
+    }
+
+    private boolean isOutOfGuesses() {
+        if (guessesleft == 0) {
+            System.out.println("No guesses left, u suck");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isWon() {
+        if (correctWord.equals(control.getLatestGuessedWord())) {
+            System.out.println("You did it! " + correctWord + " was the word I was looking for.");
+            return true;
+        }
+        return false;
+    }
     //konstruktor som tar emot en scanner.
     //instans av controller & reader
     // sout om välkommen till hangman
