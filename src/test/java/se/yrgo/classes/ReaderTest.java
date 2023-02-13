8bff4efe091264564020478c1527b264c72e4569
Christopher Lo-Martire <chrlom0905@skola.goteborg.se>
2023-02-13 10:40:28 +0100

    Adding changes to ReaderTest.java

--- src/test/java/se/yrgo/classes/ReaderTest.java
+++ src/test/java/se/yrgo/classes/ReaderTest.java
@@ -2,26 +2,26 @@ package se.yrgo.classes;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReaderTest {
-
+    List<String> wordList = new ArrayList<>();
+    wordList.add("bok");
+    
 @Test
     void testRemoveWords(){
     List<String> list = new ArrayList<>();
-    list.add("äpple");
-    list.add("banan");
-    list.add("hus");
-    list.add("hök");
-    list.add("husmor");
-    list.add("överhuvud");
+    
     
     System.out.println(list);
     
     List<String> correctList = new ArrayList<>();
     for(String word : list){
-        if()
+        if(word.contains("å") || word.contains("ä") || word.contains("ö")){
+        
+        }
     }
     
     }
