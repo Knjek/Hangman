6af1378edb1db4c2205e856615407866828b7732
Christopher Lo-Martire <chrlom0905@skola.goteborg.se>
2023-02-13 10:34:11 +0100

    Made some changes toReader.java and added a file ReaderTest.java

---
+++ src/test/java/se/yrgo/classes/ReaderTest.java
@@ -0,0 +1,28 @@
+package se.yrgo.classes;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReaderTest {
+
+@Test
+    void testRemoveWords(){
+    List<String> list = new ArrayList<>();
+    list.add("äpple");
+    list.add("banan");
+    list.add("hus");
+    list.add("hök");
+    list.add("husmor");
+    list.add("överhuvud");
+    
+    System.out.println(list);
+    
+    List<String> correctList = new ArrayList<>();
+    for(String word : list){
+        if()
+    }
+    
+    }
+}
